b2b5ea3160f827aafb0dcfd57d395aea02052853
Brent Fox <brentgfox@gmail.com>
2021-04-23 15:42:37 -0600

    Updated Github to try and fix rebase.

---
+++ src/main/java/com/ankoni/Carrera/CarreraApplication.java
@@ -0,0 +1,13 @@
+package com.ankoni.Carrera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarreraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarreraApplication.class, args);
+	}
+
+}
